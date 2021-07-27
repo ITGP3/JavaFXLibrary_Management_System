0f06b4ebbe14ee81074992a49f1266dd7a1caedb
User <User@DESKTOP-KAIHP37>
2021-07-28 00:57:56 +0630

    staff main UI

--- src/role/staff/StaffMainController.java
+++ src/role/staff/StaffMainController.java
@@ -1,4 +1,74 @@
 package role.staff;
 
-public class StaffMainController {
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import entity.UserHolder;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class StaffMainController implements Initializable{
+
+    @FXML
+    private Label lblLoginManager;
+
+    private void loadWindow(String location, String title){
+        try{
+            Stage stage = new Stage(StageStyle.DECORATED);
+            Parent root = FXMLLoader.load(getClass().getResource(location));
+            stage.setTitle(title);
+            stage.setScene(new Scene(root));
+            stage.show();
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+    
+    @FXML
+    void processBooks(MouseEvent event) {
+
+    }
+
+    @FXML
+    void processIssueBook(MouseEvent event) {
+
+    }
+
+    @FXML
+    void processLogout(MouseEvent event) throws IOException {
+
+    	 Stage staffStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+    	 staffStage.hide();
+         Parent adminRoot = FXMLLoader.load(getClass().getResource("../../main/main.fxml"));
+         staffStage.setTitle("STAFF MAIN");
+         staffStage.setScene(new Scene(adminRoot));
+         staffStage.show();
+    }
+
+    @FXML
+    void processRenewBook(MouseEvent event) {
+
+    }
+
+    @FXML
+    void processSubmissionBook(MouseEvent event) {
+
+    }
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		
+		 lblLoginManager.setText(UserHolder.getUserHolder().getEmail());
+	}
+
 }
